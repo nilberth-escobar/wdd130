91d55dc3b465e3bb74b04a5483518160860d50a4
nilberth-escobar <nilberrhescobar@gamil.com>
2023-03-16 22:32:21 -0600

    Css styles added to home page

--- practicas/scripts.js
+++ practicas/scripts.js
@@ -1,41 +1,41 @@
-let menuVisible = false;
-//Función que oculta o muestra el menu
-function mostrarOcultarMenu(){
-    if(menuVisible){
+let visibleMenu = false;
+//Show / hide menu
+function showHideMenu(){
+    if(visibleMenu){
         document.getElementById("nav").classList ="";
-        menuVisible = false;
+        visibleMenu = false;
     }else{
         document.getElementById("nav").classList ="responsive";
-        menuVisible = true;
+        visibleMenu = true;
     }
 }
 
-function seleccionar(){
+function select(){
     //oculto el menu una vez que selecciono una opcion
     document.getElementById("nav").classList = "";
-    menuVisible = false;
+    visibleMenu = false;
 }
 //Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
+function habilitiesEffect(){
     var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+    var distance_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distance_skills >= 300){
+        let habilities = document.getElementsByClassName("progress");
+        habilities[0].classList.add("javascript");
+        habilities[1].classList.add("htmlcss");
+        habilities[2].classList.add("photoshop");
+        habilities[3].classList.add("illustrator");
+        habilities[4].classList.add("drupal");
+        habilities[5].classList.add("communication");
+        habilities[6].classList.add("trabajo");
+        habilities[7].classList.add("creatividad");
+        habilities[8].classList.add("dedicacion");
+        habilities[9].classList.add("project");
     }
 }
 
 
 //detecto el scrolling para aplicar la animacion de la barra de habilidades
 window.onscroll = function(){
-    efectoHabilidades();
+    habilitiesEffect();
 } 
